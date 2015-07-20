0c3581cf9d6bf4999869970a43baff6a50c7d7ca
Dylan Howey <howey014@umn.edu>
2015-07-19 20:46:23 -0500

    changed token

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = '103338289:AAEd2VliM2OvkbcHMSZo03PDIv5ULe23oPw'
+TOKEN = '103338289:AAGr488qwMZyo4jrK7cI2fwhMAcaykfusPU'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
